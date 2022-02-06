385346da03edcfa91102a39ddd7154f842a47d25
Anton Kalinin <anton.kalinin@wolt.com>
2022-02-06 19:29:28 +0200

    v0.1.15

--- Formula/noname.rb
+++ Formula/noname.rb
@@ -2,8 +2,8 @@ class Noname < Formula
   desc "CLI tool to remove personally identifiable information from screenshots ✨"
   homepage "https://github.com/antonKalinin/noname"
   url "https://github.com/antonKalinin/noname/releases/latest/download/noname-macos.tar.gz"
-  sha256 "aa08cc3d3f9a579af3d4bf7598a43819c4789b213fd50cb11de22ee3ea585a2c"
-  version "0.1.14"
+  sha256 "e50e31463948afd493947d3ad7359afe81f1bc25943936dd0731a77bda9a20d3"
+  version "0.1.15"
 
   def install
     bin.install "noname"
